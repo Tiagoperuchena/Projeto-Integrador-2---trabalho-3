f8499cb469028ebf4f20140067e42fab571c9d64
manuelhgm <manuelmesquita.aluno@unipampa.edu.br>
2026-06-08 19:09:03 -0300

    inicio

---
+++ manuel.c
@@ -0,0 +1,37 @@
+#include "manuel.h"
+#include "funcoes.h"
+#include "struct.h"
+
+int main()
+{
+
+}
+
+void EX(typ_stt *stt)
+{
+    int16_t mux = 0;
+    typ_ulaR saida_ula = {0};
+    if (stt->ID_EX.sinal[ula_fon])
+        mux = stt->instrucao_t->immediato;
+    else
+        mux = stt->ID_EX.valor_b;
+
+    saida_ula = ula(stt->ID_EX.valor_a, mux, stt->ID_EX.ulaop);
+
+    //===========================================================
+
+    stt->EX_MEM.pc            = stt->ID_EX.pc;
+    stt->EX_MEM.pc_branch     = (stt->ID_EX.pc + stt->ID_EX.imediato);
+    //stt->EX_MEM.reg_destino   = stt->ID_EX.;
+    stt->EX_MEM.resultado_ula = saida_ula.resultado;
+    //stt->EX_MEM.sinal         = stt->ID_EX.sinal;
+    stt->EX_MEM.valor_b       = stt->ID_EX.valor_b;
+    stt->EX_MEM.zero          = saida_ula.zero;
+
+        
+}
+
+void control()
+{
+
+}
