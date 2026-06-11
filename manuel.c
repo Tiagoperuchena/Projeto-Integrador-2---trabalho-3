366bada9967e82a40fc035f927eefe17de02244d
manuelhgm <manuelmesquita.aluno@unipampa.edu.br>
2026-06-11 19:20:14 -0300

    controle em teoria pronto, só testar

--- manuel.c
+++ manuel.c
@@ -45,6 +45,9 @@ void EX(typ_stt *stt)
 
 void control(typ_stt *stt)
 {
+
+    stt->sinal[inc_pc]  = 1;
+
     switch (stt->IF_ID.instrucao.tipo)
     {
     case r: // Tipo R 
@@ -56,66 +59,70 @@ void control(typ_stt *stt)
     stt->sinal[branch]  = 0; // DVC  n sei pq mudou de nome
     stt->sinal[jump]    = 0; // DVI  tbm n sei
     
-    stt->sinal[mem_reg] = 0;
+    stt->sinal[mem_reg] = 1;
     stt->sinal[esc_reg] = 1;
 
 
-    stt->sinal[inc_pc]  = 1; 
+    
     break;
 
     case i: // Tipo I
         switch (stt->IF_ID.instrucao.opcode)
         {
         case beq:
+            stt->sinal[reg_des] = 0;
             stt->ulaop = SUB; 
+            stt->sinal[ula_fon] = 0;
+
+            stt->sinal[branch]  = 1;
+            stt->sinal[jump]    = 0;
             stt->sinal[esc_mem] = 0;
+            
             stt->sinal[esc_reg] = 0;
             stt->sinal[mem_reg] = 0;
-            stt->sinal[ula_fon] = 0;
-            stt->sinal[reg_des] = 0;
-            stt->sinal[inc_pc]  = 1;
-            stt->sinal[jump]    = 0;
-            stt->sinal[branch]  = 1;
            
         break;
             
 
         case addi:    
-            stt->ulaop = ADD;
-            stt->sinal[esc_mem] = 0;
-            stt->sinal[esc_reg] = 1;
-            stt->sinal[mem_reg] = 0;
-            stt->sinal[ula_fon] = 1;
             stt->sinal[reg_des] = 0;
-            stt->sinal[inc_pc]  = 1;
-            stt->sinal[jump]    = 0;
+            stt->ulaop = ADD;
+            stt->sinal[ula_fon] = 1;
+
             stt->sinal[branch]  = 0;
+            stt->sinal[jump]    = 0;
+            stt->sinal[esc_mem] = 0;
+            
+            stt->sinal[esc_reg] = 1;
+            stt->sinal[mem_reg] = 1;
         break;    
 
 
         case lw:
-            stt->ulaop = ADD;
-            stt->sinal[esc_mem] = 0;
-            stt->sinal[esc_reg] = 1;
-            stt->sinal[mem_reg] = 1;
-            stt->sinal[ula_fon] = 1;
             stt->sinal[reg_des] = 0;
-            stt->sinal[inc_pc]  = 1;
-            stt->sinal[jump]    = 0;
+            stt->ulaop = ADD;
+            stt->sinal[ula_fon] = 1;
+
+            stt->sinal[esc_mem] = 0;
             stt->sinal[branch]  = 0;
+            stt->sinal[jump]    = 0;
+            
+            stt->sinal[esc_reg] = 1;
+            stt->sinal[mem_reg] = 0;
         break;
 
 
         case sw:
+            stt->sinal[reg_des] = 0;
             stt->ulaop = ADD;
+            stt->sinal[ula_fon] = 1;
+
+            stt->sinal[branch]  = 0;
+            stt->sinal[jump]    = 0;
             stt->sinal[esc_mem] = 1;
+        
             stt->sinal[esc_reg] = 0;
             stt->sinal[mem_reg] = 0;
-            stt->sinal[ula_fon] = 1;
-            stt->sinal[reg_des] = 0;
-            stt->sinal[inc_pc]  = 1;
-            stt->sinal[jump]    = 0;
-            stt->sinal[branch]  = 0;
         break;
         
         default:
@@ -124,15 +131,15 @@ void control(typ_stt *stt)
     break;
 
     case j:
+        stt->sinal[reg_des] = 0;
+        stt->sinal[ula_fon] = 0;
+
+        stt->sinal[branch]  = 0;
+        stt->sinal[jump]    = 1;
         stt->sinal[esc_mem] = 0;
+        
         stt->sinal[esc_reg] = 0;
         stt->sinal[mem_reg] = 0;
-        stt->sinal[ula_fon] = 0;
-        stt->sinal[reg_des] = 0;
-        stt->sinal[inc_pc]  = 1;
-        stt->sinal[jump]    = 1;
-        stt->sinal[branch]  = 0;
-    
     default:
         break;
     }
